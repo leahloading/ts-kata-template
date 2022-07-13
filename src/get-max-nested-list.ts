3528f71cf920341d000215c7e8aa22424d4a22ec
leah <theloadingpage@gmail.com>
2022-07-13 09:54:44 +0100

    failing ts tests deliberately

---
+++ src/get-max-nested-list.ts
@@ -0,0 +1,38 @@
+
+/*
+Given a list of LIST of numbers, get max for each nested list. 
+Return the list of maximums.
+
+getMaximums([[10, 70, 30], [40], [20, -30, 80]]) → [70, 40, 80]
+
+Pseudocode:
+
+let maxValue
+let maxArray = []
+
+for each item in the inputArray 
+    find the max of item, set maxValue to max of item
+    maxArray.push(maxValue)
+end for
+
+return maxArray
+*/
+
+/**
+ * 
+ * @param {number[]} nestedList 
+ * @returns {number[]} maxArray
+ */
+ function getMaximums(nestedList: number[]): number[] {
+    let maxValue
+    let maxArray = []
+
+    for (let item of nestedList) {
+        maxValue = Math.max(item)
+        maxArray.push(maxValue)
+    }
+
+    return maxArray.length
+}
+
+console.log(getMaximums([[30,40,50],[1,2,3],[6,7,8]]),[50,3,8])
